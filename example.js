fe09276e9d3b35cceba085ea590e4d844d581bd9
凉菜 <1659488338@qq.com>
2022-06-21 14:23:02 +0800

    base function

---
+++ example.js
@@ -0,0 +1,13 @@
+const {Client} = require("./lib/client");
+const client=new Client('wxid_ory0kg6rygyk22',{log_level:'debug'})
+client.login()
+client.on('message',(e)=>{
+  if(e.content==='ping'){
+    e.reply('pong')
+  }
+  if(e.content==='你好'){
+    e.reply('你好呀')
+  }
+})
+process.stdin.on('data',()=>{})
+
